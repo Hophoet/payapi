71a8f5ada7da390df973072760eb9ec5f3eadad0
hophoet <hophoet@gmail.com>
2020-11-17 23:36:39 +0000

    register models (Client, Transfert, Method) to the django admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Client, Transfert, Method
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Transfert)
+admin.site.register(Method)

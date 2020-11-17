fe36bc982902e440901f1e45eb77d440feabb923
hophoet <hophoet@gmail.com>
2020-11-17 23:37:58 +0000

    add rest_framework and rest_auth config to the settings.py

--- tr/settings.py
+++ tr/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'rest_framework.authtoken',
+    'rest_auth',
+
     'core',
 ]
 
@@ -119,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}

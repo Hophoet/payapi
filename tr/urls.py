bd5a97859f56294936e9e6de63d4ec8822ad5746
hophoet <hophoet@gmail.com>
2020-11-17 23:40:54 +0000

    add rest_framework, rest_auth  and the core app into the projects urlspatterns

--- tr/urls.py
+++ tr/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('api/token/', obtain_auth_token, name='obtain-token'),
     path('', include('core.urls'))
 ]
